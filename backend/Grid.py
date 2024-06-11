b26432d0ddd4f1e7eaad90ba9169dd5abc1c9f23
khoale19 <khoale19012003@gmail.com>
2024-06-12 04:29:18 +0700

    .

--- backend/Grid.py
+++ backend/Grid.py
@@ -566,6 +566,7 @@ class Grid:
   
     # Launches all the events of the grid in a game's tick
     def newTickEvents(self):
+        print("logic game")
         sys = SystemAgent.get_instance()
         """
         This method updates the state of the grid. 
