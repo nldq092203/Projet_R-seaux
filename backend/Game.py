c52d6d4f0fd5ae785a5ecd7721aba2e84bdf4ad9
khoale19 <khoale19012003@gmail.com>
2024-06-12 06:57:49 +0700

    .

--- backend/Game.py
+++ backend/Game.py
@@ -160,10 +160,10 @@ class Game:
                     
 
                     # Compute the best bob, update the stats
-                    self.currentBestBob = self.grid.getBestBob()
-                    if self.currentBestBob is not None:
-                        self.bobCountHistory.append(len(self.grid.getAllBobs()))
-                        self.bestBobGenerationHistory.append(self.currentBestBob.generation)
+                    # self.currentBestBob = self.grid.getBestBob()
+                    # if self.currentBestBob is not None:
+                    #     self.bobCountHistory.append(len(self.grid.getAllBobs()))
+                    #     self.bestBobGenerationHistory.append(self.currentBestBob.generation)
                     
                 # Calculate alpha, the percentage of the tick that has passed
                 alpha = (pygame.time.get_ticks() - last_tick_time) / (1000 / Settings.maxTps)
